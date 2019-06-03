7ea1b13fb75c073190a411bca00f9ac6ec5ed5dc
nhoughto <nhoughto@gmail.com>
2019-06-04 09:41:04 +1000

    Make check interval customisable, add timestamp

--- run.sh
+++ run.sh
@@ -6,7 +6,11 @@ error_trap() {
 trap 'error_trap $LINENO' ERR
 SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+if [ -z "$CHECK_INTERVAL" ]; then
+	CHECK_INTERVAL=60
+fi
+
 while true; do
-	$SCRIPT_PATH/check.sh
-	sleep 60
+	$SCRIPT_PATH/check.sh | ts
+	sleep $CHECK_INTERVAL
 done
